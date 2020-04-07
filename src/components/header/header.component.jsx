ef488906fde313badb6c87622279cd3aceaa0a3e
avilevant <53899685+avilevant@users.noreply.github.com>
2020-04-07 22:39:32 +0300

    moving dataBase to firebase

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
@@ -10,39 +10,42 @@ import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import {HeaderContainer, LogoContainer, OptionsContainer, OptionLink} from './header.styles';
+//import './header.styles.scss';
 
-import './header.styles.scss';
+const Header = ({ currentUser, hidden }) => ( 
+  <HeaderContainer>
+    <LogoContainer to = '/' >
+    <Logo className = 'logo'/>
+    </LogoContainer> 
+    <OptionsContainer>
+    <OptionLink to = '/shop'>
+    SHOP 
+    </OptionLink> 
+    <OptionLink to = '/shop' >
+    CONTACT
+    </OptionLink> 
+    {currentUser ?( 
+    <OptionLink as='div' onClick = {() => auth.signOut()}>
+    SIGN OUT 
+    </OptionLink>
+    ) : ( 
+    <OptionLink  to = '/signin'>
+    SIGN IN 
+    </OptionLink>
+    
+   )} 
+   <CartIcon/>
+  </OptionsContainer>
+   { hidden ? null : <CartDropdown/>} 
+    
+  </HeaderContainer>
 
-const Header = ({ currentUser, hidden }) => (
-  <div className='header'>
-    <Link className='logo-container' to='/'>
-      <Logo className='logo' />
-    </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
-        SHOP
-      </Link>
-      <Link className='option' to='/shop'>
-        CONTACT
-      </Link>
-      {currentUser ? (
-        <div className='option' onClick={() => auth.signOut()}>
-          SIGN OUT
-        </div>
-      ) : (
-        <Link className='option' to='/signin'>
-          SIGN IN
-        </Link>
-      )}
-      <CartIcon />
-    </div>
-    {hidden ? null : <CartDropdown />}
-  </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
-  hidden: selectCartHidden
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 });
 
 export default connect(mapStateToProps)(Header);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,19 +2,41 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import CollectionOverview from '../../components/collections-overview/collections-overview';
 import CollectionPage from '../collection/collection';
+import {firestore, convertCollectionsSnapshotToMAp} from '../../firebase/firebase.utils';
+import {connect} from 'react-redux';
+import { updateCollections } from '../../redux/shop/shop.actions';
 
-const ShopPage = ({match }) => (
-  <div className = 'shop-page'> 
-     <Route exact path={`${match.path}`} component=  {CollectionOverview}/>
-     <Route path={`${match.path}/:collectionId`} component=  {CollectionPage}/>
-  </div>
-  );
+class ShopPage extends React.Component{
+
+  unsubscribeFromSnapshot = null;
+  
+  componentDidMount(){
+    const { updateCollections } = this.props;
+    const collectionRef = firestore.collection('collections');
+    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async => snapshot =>{
+    const collectionsMap =  convertCollectionsSnapshotToMAp(snapshot)
+    updateCollections(collectionsMap)
+    })
+  }
+
+  render(){
+    const{match} = this.props;
+    return(
+      <div className = 'shop-page'> 
+        <Route exact path={`${match.path}`} component=  {CollectionOverview}/>
+        <Route path={`${match.path}/:collectionId`} component=  {CollectionPage}/>
+      </div>
+    )
+  }
+} 
+
+const mpaDispatchToProps = dispatch =>({
+  updateCollections: collectionsMap =>
+  dispatch(updateCollections(collectionsMap))
+});
 
 
 
 
 
-
-
-
-export default ShopPage;
+export default connect(null, mpaDispatchToProps)(ShopPage);
